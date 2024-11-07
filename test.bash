71dd71ba2f3d4aef1cddfce8627a51d715b4ddcb
Ryusei Fujimura <s23c1125ja@s.chibakoudai.jp>
2024-11-07 11:41:10 +0900

    aaa

--- test.bash
+++ test.bash
@@ -15,9 +15,6 @@ out=$(echo あ | ./plus)
 [ "$?" = 1 ]      || ng "$LINENO"
 [ "${out}" = "" ] || ng "$LINENO"
 
-out=$(echo | ./plus)
-[ "$?" = 1 ]      || ng "$LINENO"
-[ "${out}" = "" ] || ng "$LINENO"
 
 [ "$res" = 0 ] && echo OK
 exit "$res"

80761873bdeb121e190873ad44fa2de33c81fd53
Ryusei Fujimura <s23c1125ja@s.chibakoudai.jp>
2024-11-23 13:14:12 +0900

    strange test

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash  -xv
+#!/bin/bash 
 
 ng () {
 	echo ${1}行目が違うよ
@@ -9,6 +9,10 @@ res=0
 
 out=$(seq 3 | ./beki)
 [ "${out}" = 64 ] || ng "$LINENO"
+
+out=$(echo あ | ./beki)
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
 
 [ "$res" = 0 ] && echo OK
 exit "$res"

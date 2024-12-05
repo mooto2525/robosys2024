08705ff4970af7f7cf86b342041f475d7a49eebe
Ryusei Fujimura <s23c1125ja@s.chibakoudai.jp>
2024-12-05 21:52:48 +0900

    finish

--- test.bash
+++ test.bash
@@ -13,24 +13,28 @@ out=$(seq 3 | ./beki)
 [ "${out}" = 64 ] || ng "$LINENO"
 
 out=$(echo あ | ./beki)
-[ "$?" = 0 ] || ng "$LINENO"
-[ "${out}" = 自然数を入力してください ] || ng "$LINENO"
+[ "$?" = 1 ] || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
 
 out=$(echo "" | ./beki)
-[ "$?" = 0 ] || ng "$LINENO"
-[ "${out}" = 自然数を入力してください ] || ng "$LINENO"
+[ "$?" = 1 ] || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
 
 out=$(echo A | ./beki)
-[ "$?" = 0 ] || ng "$LINENO"
-[ "${out}" = 自然数を入力してください ] || ng "$LINENO"
+[ "$?" = 1 ] || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
 
 out=$(echo a | ./beki)
-[ "$?" = 0 ] || ng "$LINENO"
-[ "${out}" = 自然数を入力してください ] || ng "$LINENO"
+[ "$?" = 1 ] || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
 
 out=$(echo 0 | ./beki)
 [ "$?" = 0 ] || ng "$LINENO"
 [ "${out}" = 1 ] || ng "$LINENO"
+
+out=$(echo -1 | ./beki)
+[ "$?" = 0 ] || ng "$LINENO"
+[ "${out}" = 0.5 ] || ng "$LINENO"
 
 [ "$res" = 0 ] && echo OK
 exit "$res"
